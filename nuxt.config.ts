6b8518cf4d6217ae625fc02fa5b6e070cde6e723
Ilan Yashuk <ilan147963@gmail.com>
2024-12-21 19:05:39 +0200

    Added nuxt content to project with intial code, some is from previous project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  modules: ['@nuxt/content'],
 })
---
+++ pages/[...slug].vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+        <ContentDoc>
+            <template v-slot="{ doc }">
+
+                <Head>
+                    <Meta name="og:title" :content="doc.title" />
+                    <Meta name="og:description" :content="doc.description" />
+                    <Meta name="og:image" content="" />
+                    <Meta name="og:url" content="" />
+                    <Meta name="og:type" content="article" />
+                    <Meta name="og:site_name" content="" />
+                    <Meta name="twitter:card" content="summary_large_image" />
+                    <Meta name="twitter:title" :content="doc.title" />
+                    <Meta name="twitter:description" :content="doc.description" />
+                    <Meta name="twitter:image" content="" />
+
+                </Head>
+                <article class="container">
+                        <h1 dir="auto">{{ doc.title }}</h1>
+                    <ContentRenderer :value="doc" />
+                </article>
+            </template>
+            <template #empty>
+                <h1>Document not found</h1>
+                <p>The page you were trying to access wasn't found.</p>
+            </template>
+        </ContentDoc>
+
+    </main>
+</template>
+
+<script setup>
+const { data } = await useAsyncData('hello', () => queryContent('/hello').findOne())
+</script>
